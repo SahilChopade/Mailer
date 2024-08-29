60c8e0baac5a44e0c4d383531b3714d1078632ef
Sahil Chopade <chopade.sahil02@gmail.com>
2024-08-29 15:39:06 +0530

    changed mail

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const transporter = createTransport({
 app.post("/sendmail", async (req, res) => {
   let info = await transporter.sendMail({
     from: `${req.body.Name} <${transporter.options.auth.user}>`, // sender address
-    to: "chopadesahil2002@gmail.com", // list of receivers
+    to: "iit.sahil2024@gmail.com", // list of receivers
     subject: `${req.body.Subject}`, // Subject line
     text: "Hello world?", // plain text body
     html: `${req.body.Message} <br> Sender's Email: <strong>${req.body.Email}</strong>`, // html body
